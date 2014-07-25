4072576f099f4ecc4aa1568468faa097d7453760
macke4ever <kest.cesnavicius@gmail.com>
2014-07-25 13:59:38 +0300

    Mark harvested fields
    Harvested fields are marked with while fill colour

--- addMaps.php
+++ addMaps.php
@@ -14,7 +14,7 @@ function addMaps(){
   <?php
 
    include "dbConfig.php";
-    $r = mysql_query("SELECT `fields`.id as id,  `fields`.coordinates as coordinates, `laikina`.culture_id as culture_id, `cultures`.color as color FROM `fields` LEFT OUTER JOIN (SELECT * FROM seedings where season_id = ".$_SESSION['user_season'].") as laikina ON `laikina`.field_id = `fields`.id LEFT OUTER JOIN cultures ON `laikina`.culture_id = `cultures`.id where `fields`.farm_id = '".$_SESSION['user_farm']."'");
+    $r = mysql_query("SELECT `fields`.id as id,  `fields`.coordinates as coordinates, `laikina`.culture_id as culture_id, `cultures`.color as color, `harvestings`.date as harvest FROM `fields` LEFT OUTER JOIN (SELECT * FROM seedings where season_id = ".$_SESSION['user_season'].") as laikina ON `laikina`.field_id = `fields`.id LEFT OUTER JOIN cultures ON `laikina`.culture_id = `cultures`.id LEFT OUTER JOIN harvestings ON `fields`.id = `harvestings`.field_id where `fields`.farm_id = '".$_SESSION['user_farm']."'");
     // echo "SELECT `fields`.id as id,  `fields`.coordinates as coordinates, `seedings`.culture_id as culture_id, `cultures`.color as color FROM `fields` INNER JOIN seedings ON `seedings`.field_id = `fields`.id INNER JOIN cultures ON `seedings`.culture_id = `cultures`.id where `fields`.farm_id = '4'";
     // $r = mysql_query("SELECT id, coordinates from fields where coordinates != '' and farm_id = ".$_SESSION["user_farm"]."'");   
  
@@ -43,7 +43,11 @@ function addMaps(){
           if (!empty($field['culture_id']) && !empty($field['color'])){
             $color = $field['color'];
           }
-          echo "laukas".$field['id']." = new google.maps.Polygon({ paths: polygonCoords, strokeColor: '".$color."', strokeOpacity: 0.8, strokeWeight: 2, fillColor: '".$color."', fillOpacity: 0.35, id: ".$field['id']." }); laukai.push(laukas".$field['id'].");\n";}
+          $color2 = $color;
+          if (!empty($field['harvest']) && ($field['harvest']) != "0000-00-00"){
+            $color2 = "#FFFFFF";
+          }
+          echo "laukas".$field['id']." = new google.maps.Polygon({ paths: polygonCoords, strokeColor: '".$color."', strokeOpacity: 0.8, strokeWeight: 2, fillColor: '".$color2."', fillOpacity: 0.35, id: ".$field['id']." }); laukai.push(laukas".$field['id'].");\n";}
 
     echo"\n";
       foreach($fields as $key => $field)
